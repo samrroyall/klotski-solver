eca520630e8430922b18d301d448235eb59a8c87
Sam Royall <samrroyall@gmail.com>
2020-10-02 23:36:30 +0100

    Added alert telling users how to use the app! 🔥🚀

--- src/App.js
+++ src/App.js
@@ -685,9 +685,44 @@ class Content extends React.Component {
   }
 }
 
+function removeOverlay() {
+  document.getElementById("overlay").classList.add("hide");
+}
+
 function App() {
+  const instructionsAlert = (
+    <div id="overlay">
+      <div className="alert alert-danger alert-dismissible fade show alert-body"
+        role="alert">
+            <div className="h4">Welcome to the Klotski Solver!</div>
+            <hr/>
+            The goal of Klotski is to create a board with <strong>one</strong> red block and 
+            <strong> two</strong> empty spaces and attempt to move the red block to the winning 
+            position (the pink spaces) in as few moves as possible.
+            <hr/>
+            First, click on the board to set your winning position. 
+            <br/><br/>
+            Next, drag-and-drop (desktop only) or click-to-select and click-to-place blocks
+            on the board.
+            <br/><br/>
+            Finally, click 'Solve' and watch our super cool algorithm find an optimal solution
+            to your board. Step though the solution by pressing 'Next' or 'Prev', or simply
+            press 'Finish' and watch as the board is solved before your eyes.
+            <button type="button" 
+              className="close"
+              data-dismiss="alert"
+              aria-label="Close"
+              onClick={removeOverlay}>
+                <span aria-hidden="true">&times;</span>
+            </button>
+      </div>
+    </div>
+  );
   return (
-    <Content />   
+    <div>
+      {instructionsAlert}
+      <Content />   
+    </div>
   );
 }
 
