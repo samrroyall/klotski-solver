8e97a239c960b0ae95d4696ae633fc2a8bdd4179
Sam Royall <samrroyall@gmail.com>
2021-02-17 03:14:19 +0000

    fixed overlay opacity error and edited alert language

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import '../styles/Alert.css';
 
 let Alert = () => (
     <div id="overlay">
-        <div className="alert alert-danger alert-dismissible fade show alert-body" role="alert">
+        <div className="alert alert-danger alert-dismissible fade show ml-auto mr-auto alert-body" role="alert">
             <button type="button" 
                 className="close alert-button"
                 data-dismiss="alert"
@@ -15,16 +15,17 @@ let Alert = () => (
 
             <div className="h4">Welcome to the Klotski Solver!</div>
             <hr/>
-            The goal of Klotski is to create a board with <strong>one</strong> red block and 
-            <strong> two</strong> empty spaces and attempt to move the red block to the winning 
-            position (the pink spaces) in as few moves as possible.
+            To create a Klotski puzzle, one must cover the board with <strong>exactly one</strong> red block
+            and a set of other blocks such that there are <strong>exactly two</strong> uncovered spaces. The goal
+            of the puzzle is to move the red block to the winning position (the pink spaces) in as few moves
+            as possible.
             <hr/>
-            First, click on the board to set your winning position. 
-            <br/><br/>
-            Next, drag-and-drop (desktop only) or click-to-select and click-to-place blocks
+            1) Click on the board to set your winning position. 
+            <br/>
+            2) Drag-and-drop (desktop only) or click-to-select and click-to-place blocks
             on the board.
-            <br/><br/>
-            Finally, click 'Solve' and watch our super cool algorithm find an optimal solution
+            <br/>
+            3) Finally, click 'Solve' and watch our super cool algorithm find an optimal solution
             to your board. Step though the solution by pressing 'Next' or 'Prev', or simply
             press 'Finish' and watch as the board is solved before your eyes.
         </div>
